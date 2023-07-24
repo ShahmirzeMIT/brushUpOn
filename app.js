7f85e99374f257ec7a928342f5265dd271c8b6a0
Shahmirze <sahmirze.memmedyarov@gmail.com>
2023-07-24 14:19:28 +0400

    ad swiper again

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ let swiper = new Swiper(".mySwiper", {
       var currentTranslate = swiper.translate;
       
       // Set the maximum limit for translation
-      var maxTranslate = -540;
+      var maxTranslate = -400;
       
       // If the current translate value exceeds the maximum limit, set it to the maximum
       if (currentTranslate < maxTranslate) {
